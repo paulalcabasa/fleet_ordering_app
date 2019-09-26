fb0f1f1b505a173e835af727b77c69966287f798
Paul Alcabasa <paulalcabasa@gmail.com>
2019-09-26 17:38:58 +0800

    remove add in po schedule

--- resources/views/purchase_order/po_overview.blade.php
+++ resources/views/purchase_order/po_overview.blade.php
@@ -241,8 +241,8 @@
                                     </tfoot>
                                 </table> 
 
-                            </div>
-                            <div class="card-footer">
+                            </div> 
+                            <div class="card-footer" v-if="action == 'validate'">>
                                 <button type="button" @click="addRowSched" class="btn btn-primary btn-sm">Add</button>
                                 <button type="button" @click="saveDeliverySched" class="btn btn-primary btn-sm">Save</button>
                             </div>
