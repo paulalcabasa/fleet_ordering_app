b4fa70299cb7c9f1ef16d9d471d913cecfb1561b
Paul Alcabasa <paulalcabasa@gmail.com>
2020-03-05 08:27:10 +0800

    change fpc formula

--- resources/views/price_confirmation/price_confirmation_details.blade.php
+++ resources/views/price_confirmation/price_confirmation_details.blade.php
@@ -840,7 +840,7 @@
             },
             calculateNetCost(){
                 //return parseFloat(this.calculateCost) + parseFloat(this.curModel.promo);
-                return parseFloat(this.curModel.wholesale_price) - parseFloat(this.curModel.fleet_price) + parseFloat(this.curModel.lto_registration) + parseFloat(this.sumFreebies);
+                return parseFloat(this.curModel.wholesale_price) - parseFloat(this.curModel.fleet_price) + parseFloat(this.curModel.lto_registration) + parseFloat(this.sumFreebies) + parseFloat(this.calculateMargin);
             },
             calculateSubsidy(){
                 return (parseFloat(this.calculateNetCost) - parseFloat(this.curModel.fleet_price));
